1302a6fc69658bd302ded6dac2b67fe409340d26
Thiago Oliveira <thiago.henrique@softplan.com.br>
2021-12-12 16:56:12 -0300

    Commit inicial

---
+++ src/main/java/br/com/desafio/votacaobackend/VotacaobackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.votacaobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacaobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacaobackendApplication.class, args);
+	}
+
+}
